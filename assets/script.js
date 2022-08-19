807145575a66fc122eb068f24abe8d343b3be58d
Xavier Brigode <xavier.brigode@gmail.com>
2022-08-19 16:27:50 +0200

    image city background, improve code, css start

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,17 @@ http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
 let inputField = document.getElementById("search")
 let htmlContainer = document.querySelector("main")
 let htmlCity = document.createElement("h2")
+htmlCity.classList.add("city")
 let htmlTemp = document.createElement("div")
+htmlTemp.classList.add("Temp")
+let htmlAuthor = document.createElement("div")
+htmlAuthor.classList.add("author")
 let latitude
 let longitude
 let city
 let temperature
+let author
+let picture
 
 async function submit(){
     inputField = document.getElementById("search").value
@@ -18,30 +24,33 @@ async function submit(){
     latitude = cooObject[0].lat
     longitude = cooObject[0].lon
     getWeather()
-    // console.log(cooObject)
 }
 
 async function getWeather(){
-    console.log(`${inputField} a une latitude de ${latitude}° et une longitude de ${longitude}°`)
     let weaRequest = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&cnt=1&units=metric&lang=fr&appid=8aecc0a6e3f1adb9b4ca9a87e4db5cb6`)
     let weaObject = await weaRequest.json()
-    console.log(weaObject)
     city = weaObject.city.name
     temperature = weaObject.list[0].main.temp
-    console.table(city)
-    console.table(weaObject.list[0].main.temp)
+    getImage()
+}
+
+async function getImage(){
+    let imgRequest = await fetch (`https://api.unsplash.com/search/photos?query=${inputField}&orientation=landscape&per_page=5&client_id=t7OBdnBcNXemrBLnRVvtIsncn9UShFUv5Jph7fCnqdQ`)
+    let imgObject = await imgRequest.json()
+    console.log(imgObject)
+    let randomNum = Math.floor(Math.random() * 5)
+    console.log(randomNum)
+    author = imgObject.results[randomNum].user.name
+    picture = imgObject.results[randomNum].urls.regular
     createElem()
 }
 
 function createElem(){
-
     htmlCity.innerHTML = inputField
     htmlTemp.innerHTML = temperature
+    htmlAuthor.innerHTML = author
     htmlContainer.appendChild(htmlCity)
     htmlContainer.appendChild(htmlTemp)
-}
-
-function check(){
-    console.log(latitude)
-    console.log(temperature)
+    htmlContainer.appendChild(htmlAuthor)
+    document.body.style.backgroundImage = `url('${picture}')`
 }
