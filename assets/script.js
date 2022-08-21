e388ac2d4097d88ddd7951b1239fd13e965e0fc0
Xavier Brigode <xavier.brigode@gmail.com>
2022-08-22 00:48:50 +0200

    fix js bugs + design rework

--- assets/script.js
+++ assets/script.js
@@ -9,17 +9,26 @@ let htmlTopRight = document.querySelector(".top--right")
 let htmlFooter = document.querySelector("footer")
 let htmlCity = document.createElement("h3")
 let htmlTemp = document.createElement("div")
-let htmlAuthor = document.createElement("div")
 let htmlWeather = document.createElement("div")
+let htmlBotLeft = document.querySelector(".bot--left")
+let htmlBotRight = document.querySelector(".bot--right")
+let htmlHumidity = document.createElement("div")
+let htmlWind = document.createElement("div")
+let htmlWeatherIcon = document.createElement("img")
+let htmlDescription = document.createElement("div")
+
 
 let latitude
 let longitude
 let city
 let temperature
 let weather
-let author
 let picture
 let color
+let humidity 
+let wind
+let weaIcon
+let description
 
 inputField.addEventListener("keyup", function(event){
     if (event.key === "Enter") {
@@ -42,7 +51,11 @@ async function getWeather(){
     console.log(weaObject)
     temperature = weaObject.list[0].main.temp
     weather = weaObject.list[0].weather[0].main
-    console.log(weather)
+    weaIcon = weaObject.list[0].weather[0].icon
+    description = weaObject.list[0].weather[0].description
+    humidity = weaObject.list[0].main.humidity
+    wind = weaObject.list[0].wind.speed
+    console.log(weaIcon)
     getImage()
 }
 
@@ -50,12 +63,9 @@ async function getImage(){
     let imgRequest = await fetch (`https://api.unsplash.com/search/photos?query=${inputField}&orientation=landscape&per_page=5&client_id=t7OBdnBcNXemrBLnRVvtIsncn9UShFUv5Jph7fCnqdQ`)
     let imgObject = await imgRequest.json()
     let randomNum = Math.floor(Math.random() * 5)
-    author = imgObject.results[randomNum].user.name
     picture = imgObject.results[randomNum].urls.regular
     color = imgObject.results[randomNum].color
     createElem()
-    console.log(imgObject.results[randomNum])
-
 }
 
 function createElem(){
@@ -65,15 +75,32 @@ function createElem(){
     htmlTemp.classList.add("temp")
     htmlWeather.innerHTML = weather
     htmlWeather.classList.add("weather")
-    htmlAuthor.innerHTML = author
-    htmlAuthor.classList.add("author")
+    console.log(humidity)
+    htmlHumidity.innerHTML = `Humidity : ${humidity}%`
+    htmlHumidity.classList.add("humidity")
+    htmlWind.innerHTML = `Wind speed : ${wind}m/s`
+    htmlWind.classList.add("wind")
+    htmlWeatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${weaIcon}@2x.png`)
+    htmlWeatherIcon.classList.add("weaIcon")
+    htmlDescription.innerHTML = description
+    htmlDescription.classList.add("description")
+
     htmlTopLeft.appendChild(htmlCity)
     htmlTopLeft.appendChild(htmlTemp)
     htmlTopRight.appendChild(htmlWeather)
-    htmlHeader.appendChild(htmlAuthor)
-    document.body.style.backgroundImage = `url('${picture}')`
-    console.log(color)
+    htmlBotLeft.appendChild(htmlHumidity)
+    htmlBotLeft.appendChild(htmlWind)
+    htmlBotRight.appendChild(htmlWeatherIcon)
+    htmlBotRight.appendChild(htmlDescription)
+    
+
+    
     document.querySelector(".top").style.backgroundColor = color
+    document.querySelector(".bot").style.backgroundColor = color
+    document.querySelector("footer").style.backgroundColor = color
+
+    document.body.style.backgroundImage = `url('${picture}')`
+
     // "linear-gradient(0.25turn, rgba(" + color + ", $alpha: 0.7), rgba(" + color + ", $alpha: 0.1), rgba(" + color + ", $alpha: 0.7)"
     // linear-gradient(0.25turn, rgba(${color}, $alpha: 0.7), rgba(${color}, $alpha: 0.1),rgba(${color}, $alpha: 0.7));
 }
