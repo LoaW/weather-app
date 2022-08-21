e46d1e53bd512cead7efd2b3fe85baccced8958c
Xavier Brigode <xavier.brigode@gmail.com>
2022-08-21 21:02:15 +0200

    responsive design, adjustement css, refactor js

--- assets/script.js
+++ assets/script.js
@@ -3,19 +3,29 @@ http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
 
 */
 let inputField = document.getElementById("search")
-let htmlContainer = document.querySelector("main")
-let htmlCity = document.createElement("h2")
-htmlCity.classList.add("city")
+let htmlHeader = document.querySelector("header")
+let htmlTopLeft = document.querySelector(".top--left")
+let htmlTopRight = document.querySelector(".top--right")
+let htmlFooter = document.querySelector("footer")
+let htmlCity = document.createElement("h3")
 let htmlTemp = document.createElement("div")
-htmlTemp.classList.add("Temp")
 let htmlAuthor = document.createElement("div")
-htmlAuthor.classList.add("author")
+let htmlWeather = document.createElement("div")
+
 let latitude
 let longitude
 let city
 let temperature
+let weather
 let author
 let picture
+let color
+
+inputField.addEventListener("keyup", function(event){
+    if (event.key === "Enter") {
+        submit()
+    }
+})
 
 async function submit(){
     inputField = document.getElementById("search").value
@@ -27,30 +37,43 @@ async function submit(){
 }
 
 async function getWeather(){
-    let weaRequest = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&cnt=1&units=metric&lang=fr&appid=8aecc0a6e3f1adb9b4ca9a87e4db5cb6`)
+    let weaRequest = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&cnt=1&units=metric&appid=8aecc0a6e3f1adb9b4ca9a87e4db5cb6`)
     let weaObject = await weaRequest.json()
-    city = weaObject.city.name
+    console.log(weaObject)
     temperature = weaObject.list[0].main.temp
+    weather = weaObject.list[0].weather[0].main
+    console.log(weather)
     getImage()
 }
 
 async function getImage(){
     let imgRequest = await fetch (`https://api.unsplash.com/search/photos?query=${inputField}&orientation=landscape&per_page=5&client_id=t7OBdnBcNXemrBLnRVvtIsncn9UShFUv5Jph7fCnqdQ`)
     let imgObject = await imgRequest.json()
-    console.log(imgObject)
     let randomNum = Math.floor(Math.random() * 5)
-    console.log(randomNum)
     author = imgObject.results[randomNum].user.name
     picture = imgObject.results[randomNum].urls.regular
+    color = imgObject.results[randomNum].color
     createElem()
+    console.log(imgObject.results[randomNum])
+
 }
 
 function createElem(){
     htmlCity.innerHTML = inputField
-    htmlTemp.innerHTML = temperature
+    htmlCity.classList.add("city")
+    htmlTemp.innerHTML = `${Math.floor(temperature)}°`
+    htmlTemp.classList.add("temp")
+    htmlWeather.innerHTML = weather
+    htmlWeather.classList.add("weather")
     htmlAuthor.innerHTML = author
-    htmlContainer.appendChild(htmlCity)
-    htmlContainer.appendChild(htmlTemp)
-    htmlContainer.appendChild(htmlAuthor)
+    htmlAuthor.classList.add("author")
+    htmlTopLeft.appendChild(htmlCity)
+    htmlTopLeft.appendChild(htmlTemp)
+    htmlTopRight.appendChild(htmlWeather)
+    htmlHeader.appendChild(htmlAuthor)
     document.body.style.backgroundImage = `url('${picture}')`
+    console.log(color)
+    document.querySelector(".top").style.backgroundColor = color
+    // "linear-gradient(0.25turn, rgba(" + color + ", $alpha: 0.7), rgba(" + color + ", $alpha: 0.1), rgba(" + color + ", $alpha: 0.7)"
+    // linear-gradient(0.25turn, rgba(${color}, $alpha: 0.7), rgba(${color}, $alpha: 0.1),rgba(${color}, $alpha: 0.7));
 }
